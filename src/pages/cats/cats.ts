af91a0ab39ab37e15060ad7cb2b96f5d8fcca5d5
thegreyluk <l.vogelsberger@innfactory.de>
2025-08-04 14:20:38 +0200

    feat: add main pages home, cats, favourites

---
+++ src/pages/cats/cats.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cats',
+  imports: [],
+  templateUrl: './cats.html',
+  styleUrl: './cats.scss'
+})
+export class Cats {
+
+}
