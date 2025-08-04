25d9548a90e2c0fe4e5d61520cff97f02cabc87a
thegreyluk <l.vogelsberger@innfactory.de>
2025-08-04 15:53:26 +0200

    feat: use of page content component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Content } from '../components/content/content';
 import { Footer } from '../components/footer/footer';
 import { Header } from '../components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [Header, Content, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
---
+++ src/components/content/content.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-content',
+  imports: [RouterOutlet],
+  templateUrl: './content.html',
+  styleUrl: './content.scss',
+})
+export class Content {}
