17e1f13519a11bf8cd6505f378c7c54776e39f17
thegreyluk <l.vogelsberger@innfactory.de>
2025-08-04 15:47:55 +0200

    feat: chuck norris joke overview with generation card

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-import { Header } from '../../components/header/header';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { GenerationCard } from '../../components/generation-card/generation-card';
 @Component({
   selector: 'app-home',
-  imports: [Header],
+  imports: [MatButtonModule, MatProgressBarModule, MatIconModule, GenerationCard],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home {
+  longText = 'Chuck Norris hat einen Grizzlybären Vorleger in seinem Zimmer. Der Bär ist nicht tot, er hat nur Angst sich zu bewegen.';
+}
