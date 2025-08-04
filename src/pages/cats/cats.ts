62f124371a2a1ff7ad60c189e7d7d16f5d571c35
thegreyluk <l.vogelsberger@innfactory.de>
2025-08-04 15:52:49 +0200

    feat: cat overview with generation card

--- src/pages/cats/cats.ts
+++ src/pages/cats/cats.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { GenerationCard } from '../../components/generation-card/generation-card';
 
 @Component({
   selector: 'app-cats',
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, GenerationCard],
   templateUrl: './cats.html',
-  styleUrl: './cats.scss'
+  styleUrl: './cats.scss',
 })
 export class Cats {
-
+  longText = 'Chuck Norris hat einen Grizzlybären Vorleger in seinem Zimmer. Der Bär ist nicht tot, er hat nur Angst sich zu bewegen.';
 }
