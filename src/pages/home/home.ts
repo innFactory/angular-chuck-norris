0a995c0884a52589fcff288407d609a60bfc1232
thegreyluk <l.vogelsberger@innfactory.de>
2025-08-05 17:06:34 +0200

    feat: use favourite data service in jokes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,6 +4,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { GenerationCard } from '../../components/generation-card/generation-card';
 import { ChuckNorrisJoke } from '../../models/chuck-norris-joke';
+import { AllowedDataTypes } from '../../models/favourite-data';
+import { FavouritesDataService } from '../../shared/services/favourites-data/favourites-data';
 import { JokeFetcher } from '../../shared/services/joke-fetcher/joke-fetcher';
 @Component({
   selector: 'app-home',
@@ -14,6 +16,7 @@ import { JokeFetcher } from '../../shared/services/joke-fetcher/joke-fetcher';
 export class Home {
   private jokeFetcherService = inject(JokeFetcher);
   protected jokeResource: ResourceRef<ChuckNorrisJoke | undefined> = resource({ loader: () => this.jokeFetcherService.get() });
+  private favouritesDataService = inject(FavouritesDataService);
 
   protected get jokeString(): string {
     const jokeResourceValue = this.jokeResource.value();
@@ -22,5 +25,9 @@ export class Home {
 
   protected fetchNewJoke() {
     this.jokeResource.reload();
+  }
+
+  protected favouriseJoke() {
+    this.favouritesDataService.add(this.jokeResource.value()?.value ?? '', AllowedDataTypes.Joke);
   }
 }
