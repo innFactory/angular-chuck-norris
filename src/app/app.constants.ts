cefd953577618da88f817ec89622dab170fdabc0
thegreyluk <l.vogelsberger@innfactory.de>
2025-08-05 13:00:56 +0200

    feat: add api URLs

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,2 +1,6 @@
 // External links
-export const LINK_GITHUB_REPO = 'https://github.com/innFactory/angular-chuck-norris';
+export const URL_GITHUB_REPO = 'https://github.com/innFactory/angular-chuck-norris';
+
+// API URLs
+export const URL_CHUCK_NORRIS_API = 'https://api.chucknorris.io/jokes/random';
+export const URL_CAT_API = 'https://cataas.com/cat?json=true';
