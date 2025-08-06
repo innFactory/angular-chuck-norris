ccaf5d42d1b0fb47c840af1782f39f93c29d4dc0
thegreyluk <l.vogelsberger@innfactory.de>
2025-08-06 15:15:42 +0200

    feat: implement firebase connection

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 import { Content } from '../components/content/content';
 import { Footer } from '../components/footer/footer';
 import { Header } from '../components/header/header';
@@ -9,4 +10,15 @@ import { Header } from '../components/header/header';
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {}
+export class App {
+  events$ = signal<any[]>([]); // Signal, um die Ereignisse zu speichern
+  firestore = inject(Firestore);
+
+  ngOnInit() {
+    const eventsCollection = collection(this.firestore, 'favourites');
+    collectionData(eventsCollection).subscribe((data) => {
+      this.events$.set(data);
+      console.log(this.events$());
+    });
+  }
+}
