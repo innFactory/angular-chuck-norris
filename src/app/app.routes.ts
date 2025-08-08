37f1e392a882da5ce50ccfbd867fd65e45a82084
thegreyluk <l.vogelsberger@innfactory.de>
2025-08-08 10:55:32 +0200

    refactor: update application route to match current setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,21 @@ import { Routes } from '@angular/router';
 import { Cats } from '../pages/cats/cats';
 import { Favourites } from '../pages/favourites/favourites';
 import { Home } from '../pages/home/home';
+import { Jokes } from '../pages/jokes/jokes';
 
 export const routes: Routes = [
   {
     path: '',
+    pathMatch: 'full',
+    redirectTo: '/home',
+  },
+  {
+    path: 'home',
     component: Home,
+  },
+  {
+    path: 'jokes',
+    component: Jokes,
   },
   {
     path: 'favourites',
