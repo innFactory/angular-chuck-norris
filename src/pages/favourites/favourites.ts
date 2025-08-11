1c16c0e60a4e2dd0c7923af206d570add517be91
thegreyluk <l.vogelsberger@innfactory.de>
2025-08-11 13:40:50 +0200

    feat: trunc of method deleteRow for apprentice implementation

--- src/pages/favourites/favourites.ts
+++ src/pages/favourites/favourites.ts
@@ -1,4 +1,7 @@
 import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
 import { AllowedDataTypes } from '../../models/favourite-chuck-joke';
 import { FavouritesTableRow } from '../../models/favourites-table-row';
@@ -6,12 +9,12 @@ import { FavouriteChuckJokesSevice } from '../../shared/services/favourite-chuck
 
 @Component({
   selector: 'app-favourites',
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './favourites.html',
   styleUrl: './favourites.scss',
 })
 export class Favourites {
-  protected displayedColumns: string[] = ['identifier', 'joke', 'creator'];
+  protected displayedColumns: string[] = ['identifier', 'joke', 'creator', 'action'];
   protected tableData: FavouritesTableRow[] = [];
   private favouriteChuckJokesSevice = inject(FavouriteChuckJokesSevice);
   protected AllowedDataTypes = AllowedDataTypes;
@@ -31,5 +34,9 @@ export class Favourites {
       };
       this.tableData.push(row);
     });
+  }
+
+  protected deleteRow(id: number) {
+    console.log('Deletion not yet implemented.');
   }
 }
