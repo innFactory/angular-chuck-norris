43ca9e7a1cb82be376ec3653f78a98b73b69bc16
thegreyluk <l.vogelsberger@innfactory.de>
2025-08-11 13:21:29 +0200

    refactor: move chuck-jokes to own component

--- src/pages/jokes/jokes.ts
+++ src/pages/jokes/jokes.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { ChuckJokesCard } from '../../components/chuck-jokes-card/chuck-jokes-card';
 
 @Component({
   selector: 'app-jokes',
-  imports: [MatButtonModule, MatIconModule, ChuckJokesCard],
+  imports: [MatButtonModule, MatIconModule, ChuckJokesCard, MatFormFieldModule, MatInputModule],
   templateUrl: './jokes.html',
   styleUrl: './jokes.scss',
 })
