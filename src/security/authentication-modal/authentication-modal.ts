dccaa2b4503de9c5b7d95c5c88791fe4f9268ef2
thegreyluk <l.vogelsberger@innfactory.de>
2025-08-18 15:00:43 +0200

    feat: implement authentication modal. modularized login and register

--- src/security/authentication-modal/authentication-modal.ts
+++ src/security/authentication-modal/authentication-modal.ts
@@ -1,19 +1,30 @@
-import { Component, inject } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, Inject, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
 import { AuthService } from '../authentication/authentication';
 import { Login } from '../login/login/login';
+
 import { Register } from '../register/register/register';
+
+export enum AuthenticationOptions {
+  login = 'login',
+  signin = 'signin ',
+}
+
+export interface AuthenticationModalProps {
+  options: AuthenticationOptions;
+}
 
 @Component({
   selector: 'app-authentication-modal',
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, Login, Register],
+  imports: [MatDialogModule, MatInputModule, MatButtonModule, Login, Register],
   templateUrl: './authentication-modal.html',
   styleUrl: './authentication-modal.scss',
 })
 export class AuthenticationModal {
+  @Inject(MAT_DIALOG_DATA) public data: AuthenticationModalProps | undefined;
+
   private authService = inject(AuthService);
 
   protected async logout() {
