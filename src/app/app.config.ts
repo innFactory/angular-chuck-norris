6562c94fe98ddca47e8cdc138e46d66286ca4546
thegreyluk <l.vogelsberger@innfactory.de>
2025-08-18 13:09:35 +0200

    feat: provide global material button, formfield UI options

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,19 +3,26 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { MAT_BUTTON_CONFIG } from '@angular/material/button';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { provideRouter } from '@angular/router';
 import { initializeApp } from 'firebase/app';
 import { environment } from '../../environments/environment';
 import { routes } from './app.routes';
 
+const googleFirebaseProviders = [provideFirebaseApp(() => initializeApp(environment.firebase)), provideAuth(() => getAuth())];
+const googleFirestoreProviders = [provideFirestore(() => getFirestore())];
+const globalFormFieldUIOptions = { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } };
+const globalButtonUIOptions = { provide: MAT_BUTTON_CONFIG, useValue: { defaultAppearance: 'filled' } };
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideHttpClient(), //Injection for fetch
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideFirestore(() => getFirestore()),
-    provideAuth(() => getAuth()),
+    provideHttpClient(),
+    googleFirebaseProviders,
+    googleFirestoreProviders,
+    globalFormFieldUIOptions,
+    globalButtonUIOptions,
   ],
 };
