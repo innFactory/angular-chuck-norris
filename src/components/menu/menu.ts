f8984f18229abd7145f3ba336e1835ddeff0ed53
thegreyluk <l.vogelsberger@innfactory.de>
2025-08-18 15:01:10 +0200

    feat: use of menu for authentication opening

--- src/components/menu/menu.ts
+++ src/components/menu/menu.ts
@@ -1,7 +1,9 @@
 import { Component, computed, inject, Renderer2 } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { AuthenticationModal, AuthenticationModalProps, AuthenticationOptions } from '../../security/authentication-modal/authentication-modal';
 import { AuthService } from '../../security/authentication/authentication';
 
 @Component({
@@ -14,6 +16,8 @@ export class Menu {
   private renderer = inject(Renderer2);
   private authService = inject(AuthService);
   protected isAuthenticated = computed(this.authService.isAuthenticated);
+  protected AuthenticationOptions = AuthenticationOptions;
+  private readonly matDialogService = inject(MatDialog);
 
   protected toggleLightDarkTheme() {
     const body = document.body;
@@ -23,6 +27,13 @@ export class Menu {
     } else {
       this.renderer.addClass(body, 'activate-light-mode');
     }
+  }
+
+  protected openModalAuthentication(modalType: AuthenticationOptions) {
+    const dialogConfig = new MatDialogConfig<AuthenticationModalProps>();
+    dialogConfig.autoFocus = false;
+    dialogConfig.data = { options: modalType };
+    this.matDialogService.open(AuthenticationModal, dialogConfig);
   }
 
   protected logout() {
