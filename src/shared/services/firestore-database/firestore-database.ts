728b00c5a6acdbf0982b1520daf8cf2c7cd19dd4
thegreyluk <l.vogelsberger@innfactory.de>
2025-08-19 14:24:35 +0200

    feat: implement firestore database access. add, remove, get jokes for user

---
+++ src/shared/services/firestore-database/firestore-database.ts
@@ -0,0 +1,45 @@
+import { inject, Injectable } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
+import { addDoc, collection, deleteDoc, doc, Firestore, getDocs } from '@angular/fire/firestore';
+import { FirestoreChuckJokes } from '../favourite-chuck-jokes/favourite-chuck-jokes-firestore';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FirestoreDatabaseService {
+  private firestore = inject(Firestore);
+  private auth = inject(Auth);
+
+  public async addJoke(joke: string): Promise<void> {
+    const userId = this.auth.currentUser?.uid;
+    if (!userId) throw new Error('User not logged in.');
+    const jokesCollection = collection(this.firestore, `users/${userId}/jokes`);
+    const jokeData = { tableID: Math.ceil(100 * Math.random()), content: joke, creator: 'Chuck' };
+    await addDoc(jokesCollection, jokeData);
+  }
+
+  public async removeJoke(tableID: number): Promise<void> {
+    const jokes = await this.getAllJokes();
+    const jokeToRemove = jokes.find((joke) => joke.tableID === tableID);
+    if (jokeToRemove?.id == undefined) return;
+
+    const userId = this.auth.currentUser?.uid;
+    if (!userId) throw new Error('User not logged in.');
+    const jokeDocRef = doc(this.firestore, `users/${userId}/jokes/${jokeToRemove.id}`);
+    await deleteDoc(jokeDocRef);
+  }
+
+  public async getAllJokes(): Promise<FirestoreChuckJokes[]> {
+    const userId = this.auth.currentUser?.uid;
+    if (!userId) throw new Error('User not logged in.');
+    const jokesCollection = collection(this.firestore, `users/${userId}/jokes`);
+    const querySnapshot = await getDocs(jokesCollection);
+    const jokes: any[] = [];
+    querySnapshot.forEach((doc) => {
+      const data = doc.data();
+      jokes.push({ id: doc.id, ...data });
+    });
+    console.log(jokes);
+    return jokes;
+  }
+}
