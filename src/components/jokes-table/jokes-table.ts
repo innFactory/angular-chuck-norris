881b1fe72e242ff890e40a67e2a2c379284bafdf
thegreyluk <l.vogelsberger@innfactory.de>
2025-08-20 13:55:37 +0200

    refactor: modularize favourites

---
+++ src/components/jokes-table/jokes-table.ts
@@ -0,0 +1,47 @@
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { FavouritesTableRow } from '../../models/favourites-table-row';
+import { FavouriteChuckJokesSevice } from '../../shared/services/favourite-chuck-jokes/favourite-chuck-jokes-service/favourite-chuck-jokes-service';
+
+@Component({
+  selector: 'app-jokes-table',
+  imports: [MatTableModule, MatButtonModule, MatDividerModule, MatIconModule],
+  templateUrl: './jokes-table.html',
+  styleUrl: './jokes-table.scss',
+})
+export class JokesTable {
+  private favouriteChuckJokesSevice = inject(FavouriteChuckJokesSevice);
+  private snackbarService = inject(MatSnackBar);
+  protected displayedColumns: string[] = ['id', 'joke', 'creator', 'action'];
+  protected tableData: FavouritesTableRow[] = [];
+
+  constructor() {
+    this.updateChucksJokesTableData();
+  }
+
+  private updateChucksJokesTableData() {
+    const favouriteChuckJokes = this.favouriteChuckJokesSevice.getAllJokes();
+
+    favouriteChuckJokes.forEach((favourite) => {
+      const row: FavouritesTableRow = {
+        id: favourite.id,
+        joke: favourite.text,
+        creator: 'Chuck',
+      };
+      this.tableData.push(row);
+    });
+  }
+
+  protected deleteRow(index: number) {
+    this.snackbarService.open('🚨 Löschen noch nicht implementiert');
+  }
+
+  protected refreshTable() {
+    this.tableData = [];
+    this.updateChucksJokesTableData();
+  }
+}
