71227b741b33d7f0fc0e02f7c50080dc350f9560
thegreyluk <l.vogelsberger@innfactory.de>
2025-08-22 14:34:00 +0200

    feat: firestore joke service. use better document IDs

--- src/shared/services/joke-database-service/joke-database-service.ts
+++ src/shared/services/joke-database-service/joke-database-service.ts
@@ -1,45 +1,52 @@
 import { inject, Injectable } from '@angular/core';
 import { Auth } from '@angular/fire/auth';
-import { addDoc, collection, deleteDoc, doc, Firestore, getDocs } from '@angular/fire/firestore';
+import { collection, deleteDoc, doc, Firestore, getDocs, setDoc } from '@angular/fire/firestore';
 import { FirebaseJokeTableRow } from '../../../models/firebase-joke-table-row';
+import { AuthService } from '../../../security/auth/auth-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class JokeDatabaseService {
+  private authService = inject(AuthService);
   private firestore = inject(Firestore);
   private auth = inject(Auth);
 
-  public async add(tableID: string, joke: string): Promise<void> {
-    const userId = this.auth.currentUser?.uid;
-    if (!userId) throw new Error('User not logged in.');
-    const jokesCollection = collection(this.firestore, `users/${userId}/jokes`);
-    const jokeData = { tableID: tableID, content: joke, creator: 'Chuck' };
-    await addDoc(jokesCollection, jokeData);
+  public async add(jokeID: string, joke: string): Promise<void> {
+    if (!this.authService.checkAuthenticated()) {
+      throw new Error('User not logged in.');
+    }
+    if (this.auth.currentUser === null) return;
+    const userTableDocumentID = this.auth.currentUser.email;
+    const jokesCollection = collection(this.firestore, `users/${userTableDocumentID}/jokes`);
+    const jokeData = { content: joke, creator: 'Chuck' };
+    const jokeDocRef = doc(jokesCollection, jokeID);
+    await setDoc(jokeDocRef, jokeData);
   }
 
-  public async remove(tableID: string): Promise<void> {
-    const jokes = await this.getAll();
-    const jokeToRemove = jokes.find((joke) => joke.tableID === tableID);
-    if (jokeToRemove?.id == undefined) return;
-
-    const userId = this.auth.currentUser?.uid;
-    if (!userId) throw new Error('User not logged in.');
-    const jokeDocRef = doc(this.firestore, `users/${userId}/jokes/${jokeToRemove.id}`);
+  public async remove(jokeID: string): Promise<void> {
+    if (!this.authService.checkAuthenticated()) {
+      throw new Error('User not logged in.');
+    }
+    if (this.auth.currentUser === null) return;
+    const userTableDocumentID = this.auth.currentUser.email;
+    const jokeDocRef = doc(this.firestore, `users/${userTableDocumentID}/jokes/${jokeID}`);
     await deleteDoc(jokeDocRef);
   }
 
   public async getAll(): Promise<FirebaseJokeTableRow[]> {
-    const userId = this.auth.currentUser?.uid;
-    if (!userId) throw new Error('User not logged in.');
-    const jokesCollection = collection(this.firestore, `users/${userId}/jokes`);
+    if (!this.authService.checkAuthenticated()) {
+      throw new Error('User not logged in.');
+    }
+    if (this.auth.currentUser === null) return [];
+    const userTableDocumentID = this.auth.currentUser.email;
+    const jokesCollection = collection(this.firestore, `users/${userTableDocumentID}/jokes`);
     const querySnapshot = await getDocs(jokesCollection);
-    const jokes: any[] = [];
+    const jokes: FirebaseJokeTableRow[] = [];
     querySnapshot.forEach((doc) => {
       const data = doc.data();
-      jokes.push({ id: doc.id, ...data });
+      jokes.push({ id: doc.id, content: data['content'], creator: data['creator'] });
     });
-    console.log(jokes);
     return jokes;
   }
 }
